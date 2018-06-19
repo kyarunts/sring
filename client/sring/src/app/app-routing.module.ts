e854a8b477add0979c66b947743b603e59fcdf93
Kyarunts David <kyaruntsdavid@gmail.com>
2018-06-19 22:35:52 +0400

    Start the project

---
+++ client/sring/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'create', component: CreateComponent },
+    { path: 'about', component: AboutComponent },
+    {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
